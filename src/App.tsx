6eac9144bd84723237b4fb0feae156e05b3df77e
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-27 22:14:48 -0300

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import { FC } from 'react';
+
+import Header from './components/Header';
+import Accordions from './components/Accordions';
 import './App.css';
+
+const accordionData = [
+  {
+    question: 'Lorem ipsum dolor sit amet 1 ?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vero quos doloremque eum quam dolorem.'
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet 2 ?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vero quos doloremque eum quam dolorem inventore minus ad. Molestias, minima! Doloribus, vero quos doloremque.'
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet 3 ?',
+    answer:
+      'Doloribus, vero quos doloremque eum quam dolorem inventore minus ad. Molestias, minima!'
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet 4 ?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vero quos doloremque eum quam dolorem inventore minus ad. Molestias, minima!'
+  }
+];
 
 const App: FC = () => {
   return (
-    <div>
-      <p>ReactJS Accordion</p>
-    </div>
+    <>
+      <Header title="React JS accordions" />
+      <div className="container">
+        <Accordions
+          closeOthers
+          data={accordionData}
+          duration={400}
+          opened={2}
+        />
+      </div>
+    </>
   );
 };
 
